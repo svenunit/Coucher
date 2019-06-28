003f2c8abdc8fef138ebb1ed5e48937141584735
Peter Mentz <petermentz03@gmail.com>
2019-06-28 23:00:59 +0200

    BasicMovement

---
+++ CoucherUnityProject/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField]
+    int PlayerNumber;
+
+    float _verticalAxes;
+    float _horizontalAxes;
+    public float movementSpeed;
+
+
+
+    private void Start()
+    {
+        string[] controller = Input.GetJoystickNames();
+        for(int i = 0; i < controller.Length; i++)
+        {
+            print(i + " " + controller[i]);
+        }
+    }
+    private void FixedUpdate()
+    {
+        _verticalAxes = Input.GetAxis("VerticalP" + PlayerNumber)*movementSpeed*Time.deltaTime;
+        _horizontalAxes = Input.GetAxis("HorizontalP" + PlayerNumber)*movementSpeed*Time.deltaTime;
+
+        transform.Translate(_horizontalAxes, _verticalAxes,0);
+
+    }
+}
