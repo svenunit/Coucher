1bc13895fb37b05958e4d15087258db1eaac0130
Peter Mentz <petermentz03@gmail.com>
2019-06-29 01:36:26 +0200

    Rotation

--- CoucherUnityProject/Assets/Scripts/PlayerInput.cs
+++ CoucherUnityProject/Assets/Scripts/PlayerInput.cs
@@ -9,7 +9,15 @@ public class PlayerInput : MonoBehaviour
 
     float _verticalAxes;
     float _horizontalAxes;
+    float _turnV;
+    float _turnH;
+    Quaternion _aimRotation;
+    
+
+    Vector2 _aimDirection;
+    float _aimAngle;
     public float movementSpeed;
+    public float rotationSpeed;
 
     [Header("DEBUG")]
     public bool keyboardMovement = false;
@@ -34,17 +42,32 @@ public class PlayerInput : MonoBehaviour
         {
             _verticalAxes = Input.GetAxis("VerticalP" + _playerNumber+"K") * movementSpeed * Time.deltaTime;
             _horizontalAxes = Input.GetAxis("HorizontalP" + _playerNumber+"K")  * movementSpeed * Time.deltaTime;
-            transform.Translate(_horizontalAxes, _verticalAxes, 0);
+            
         }
         else
         {
 
             _verticalAxes = Input.GetAxis("VerticalP" + _playerNumber) * movementSpeed * Time.deltaTime;
             _horizontalAxes = Input.GetAxis("HorizontalP" + _playerNumber) * movementSpeed * Time.deltaTime;
-            transform.Translate(_horizontalAxes, _verticalAxes, 0);
+
+            _turnV = Input.GetAxis("VerticalRightStickP" + _playerNumber);
+            _turnH = Input.GetAxis("HorizontalRightStickP" + _playerNumber);
+
+            _aimDirection = new Vector2(_turnH, _turnV);
+            _aimAngle = Mathf.Atan2(_turnH, _turnV) * Mathf.Rad2Deg;
+
 
 
         }
+     
+        transform.Translate(_horizontalAxes, _verticalAxes, 0, Space.World);
+        if (_turnV != 0 && _turnH != 0)
+
+        {
+            _aimRotation = Quaternion.AngleAxis(_aimAngle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, _aimRotation, rotationSpeed * Time.deltaTime);
+        }
+        //Debug.Log(Input.GetAxis("VerticalRightStickP" + _playerNumber) + "|"+Input.GetAxis("HorizontalRightStickP" + _playerNumber);
 
     }
     public int getPlayerNumber()
