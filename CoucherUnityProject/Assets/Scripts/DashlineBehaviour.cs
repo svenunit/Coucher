2e121e0a1e726d87a1c08970f3fc806e66668ea2
Peter Mentz <petermentz03@gmail.com>
2019-06-29 15:17:23 +0200

    !

---
+++ CoucherUnityProject/Assets/Scripts/DashlineBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashlineBehaviour : MonoBehaviour
+{
+    private void Awake()
+    {
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        MeshCollider meshCollider = gameObject.AddComponent<MeshCollider>();
+
+        Mesh mesh = new Mesh();
+        lineRenderer.BakeMesh(mesh, true);
+        meshCollider.sharedMesh = mesh;
+
+       
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+    }
+}
+
