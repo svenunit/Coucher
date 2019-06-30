fb42a4f740a33454f274aacb7a3f809ee6b963ef
Peter Mentz <petermentz03@gmail.com>
2019-06-30 15:31:45 +0200

    Sound

---
+++ CoucherUnityProject/Assets/PlayerSelect.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelect : MonoBehaviour
+{
+    public GameObject[] player;
+
+    public Sprite[] playerSprites;
+}
